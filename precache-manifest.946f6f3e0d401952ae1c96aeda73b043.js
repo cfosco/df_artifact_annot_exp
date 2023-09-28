5fb1efbe07a08cbdc7b6f27284c048944d902f2e
Camilo Fosco <camilofosco@gmail.com>
2023-09-27 23:47:07 -0400

    Updates

---
+++ precache-manifest.946f6f3e0d401952ae1c96aeda73b043.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14026f1f242e4732daa7e7bdbbc9bfa2",
+    "url": "/df_artifact_annot_exp/index.html"
+  },
+  {
+    "revision": "bb6edc61ba876a8c4e8f",
+    "url": "/df_artifact_annot_exp/static/css/2.399453d7.chunk.css"
+  },
+  {
+    "revision": "d54760575490f399db52",
+    "url": "/df_artifact_annot_exp/static/css/main.1dd51469.chunk.css"
+  },
+  {
+    "revision": "bb6edc61ba876a8c4e8f",
+    "url": "/df_artifact_annot_exp/static/js/2.892d1505.chunk.js"
+  },
+  {
+    "revision": "f29967414c78e83b3aa9e610729eee20",
+    "url": "/df_artifact_annot_exp/static/js/2.892d1505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54760575490f399db52",
+    "url": "/df_artifact_annot_exp/static/js/main.c831a523.chunk.js"
+  },
+  {
+    "revision": "ec86c7199cfd3feef4ed",
+    "url": "/df_artifact_annot_exp/static/js/runtime-main.a14a9cd5.js"
+  },
+  {
+    "revision": "524ef8d40f94d8011416c5b6cdecee49",
+    "url": "/df_artifact_annot_exp/static/media/dodont.524ef8d4.png"
+  },
+  {
+    "revision": "2fb621caf70a2517dbdbc47aa5b2e728",
+    "url": "/df_artifact_annot_exp/static/media/instr_gif_1.2fb621ca.gif"
+  },
+  {
+    "revision": "54bbf15b3ad10de39dd0e3cfe0917014",
+    "url": "/df_artifact_annot_exp/static/media/instr_gif_2.54bbf15b.gif"
+  },
+  {
+    "revision": "c8690f182b9188472a89b23b420ac25b",
+    "url": "/df_artifact_annot_exp/static/media/subtitled_instruction_vid_exp2_cut.c8690f18.mp4"
+  }
+]);
